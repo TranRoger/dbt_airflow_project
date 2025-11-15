4fd31899fe1ead82041663398f29923de7bfe35c
Roger <anhth5659@gmail.com>
2025-11-15 14:13:53 +0700

    ci: test python ci

--- airflow/dags/dbt_bronze_dag.py
+++ airflow/dags/dbt_bronze_dag.py
@@ -3,35 +3,35 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    'dbt_bronze_layer',
+    "dbt_bronze_layer",
     default_args=default_args,
-    description='Run dbt Bronze layer models',
+    description="Run dbt Bronze layer models",
     schedule_interval=timedelta(minutes=5),
     start_date=datetime(2024, 1, 1),
     catchup=False,
-    tags=['dbt', 'sqlserver', 'bronze'],
+    tags=["dbt", "sqlserver", "bronze"],
 )
 
 # Run only Bronze layer models
 dbt_run_bronze = BashOperator(
-    task_id='dbt_run_bronze',
-    bash_command='docker exec dbt_airflow_project-dbt-1 dbt run --models bronze',
+    task_id="dbt_run_bronze",
+    bash_command="docker exec dbt_airflow_project-dbt-1 dbt run --models bronze",
     dag=dag,
 )
 
 # Test Bronze layer models
 dbt_test_bronze = BashOperator(
-    task_id='dbt_test_bronze',
-    bash_command='docker exec dbt_airflow_project-dbt-1 dbt test --models bronze',
+    task_id="dbt_test_bronze",
+    bash_command="docker exec dbt_airflow_project-dbt-1 dbt test --models bronze",
     dag=dag,
 )
 
